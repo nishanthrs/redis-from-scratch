2d2afe4d57401db1c113bbba5839789318183282
Nishanth Salinamakki <nsalina@protonmail.com>
2023-10-14 21:37:13 -0400

    Finished fourth challenge; respond to multiple PINGs on same connection

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::io::{Read,Write};
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
 use anyhow::bail;
 use log::{info,debug};
 use env_logger::{Env};
@@ -7,6 +7,40 @@ use env_logger::{Env};
 
 const CHUNK_SIZE: usize = 1024;
 const NULL_BYTE: &str = "\0";
+
+fn handle_connection(stream: &mut TcpStream) -> anyhow::Result<()> {
+    let mut read_buffer = [0;CHUNK_SIZE];
+    loop {
+        let num_bytes_read = stream.read(&mut read_buffer).expect("Reading from stream into buffer failed!");
+        debug!("Num bytes read: {}", num_bytes_read);
+        if num_bytes_read == 0 {
+            break;
+        }
+
+        let request = std::str::from_utf8(&read_buffer).expect("Couldn't parse buffer into str.").split(NULL_BYTE).next();
+        info!("Stream input: {:?}", request);
+        match request {
+            Some(request) => {
+                for req_part in request.split("\r\n") {
+                    match req_part.to_uppercase().as_str() {
+                        "PING" => stream.write(b"+PONG\r\n"),
+                        "" => {
+                            debug!("Reached end of input.");
+                            Ok(0)
+                        }
+                        other_cmd => {
+                            debug!("Input: {} is currently not handled.", other_cmd);
+                            Ok(0)
+                        },
+                    }.expect("Parsing request failed!");
+                }
+            },
+            None => bail!("No data after split by null byte"),
+        }
+    }
+
+    Ok(())
+}
 
 fn main() -> anyhow::Result<()> {
     let env = Env::default().default_filter_or("debug");
@@ -18,30 +52,8 @@ fn main() -> anyhow::Result<()> {
         match stream {
             Ok(mut stream) => {
                 info!("accepted new connection");
-                let mut read_buffer = [0;CHUNK_SIZE];
-                let num_bytes_read = stream.read(&mut read_buffer).expect("Reading from stream into buffer failed!");
-                debug!("Num bytes read: {}", num_bytes_read);
-                let request = std::str::from_utf8(&read_buffer).expect("Couldn't parse buffer into str.").split(NULL_BYTE).next();
-                info!("Stream input: {:?}", request);
-
-                match request {
-                    Some(request) => {
-                        for req_part in request.split("\r\n") {
-                            match req_part.to_uppercase().as_str() {
-                                "PING" => stream.write(b"+PONG\r\n"),
-                                "" => {
-                                    debug!("Reached end of input.");
-                                    Ok(0)
-                                }
-                                other_cmd => {
-                                    debug!("Input: {} is currently not handled.", other_cmd);
-                                    Ok(0)
-                                },
-                            }.expect("Parsing request failed!");
-                        }
-                    },
-                    None => bail!("No data after split by null byte"),
-                }
+                // Within same connection, accept multiple commands in loop; if # bytes read is 0, exit connection
+                handle_connection(&mut stream).expect("Something went wrong while handling connection.");
             }
             Err(e) => {
                 bail!("error: {}", e);
